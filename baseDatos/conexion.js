4ef51903e2c09e1064e5b38b787a2d026be9da67
PabloSan1997 <jos_pab_997@hotmali.com>
2023-03-18 19:46:59 -0600

    Medidas desesperadas

--- baseDatos/conexion.js
+++ baseDatos/conexion.js
@@ -2,7 +2,7 @@
 const mongoose = require("mongoose");
 const { userDb, passwordDB, nameDB } = require("./variables");
 
-const URL = `mongodb+srv://${userDb}:${passwordDB}@cluster0.c9zjxa0.mongodb.net/${nameDB}?retryWrites=true&w=majority`;
+const URL = `mongodb+srv://pablo:1234@cluster0.c9zjxa0.mongodb.net/elementos?retryWrites=true&w=majority`;
 
 function conectar(){
     mongoose.connect(URL,  { useNewUrlParser: true, useUnifiedTopology: true });
