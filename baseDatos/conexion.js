07d032e9490b93f394b35502f619ce0ce0e480da
PabloSan1997 <jos_pab_997@hotmali.com>
2023-03-18 19:03:55 -0600

    Se borraron console.log

--- baseDatos/conexion.js
+++ baseDatos/conexion.js
@@ -5,8 +5,9 @@ const { userDb, passwordDB, nameDB } = require("./variables");
 const URL = `mongodb+srv://${userDb}:${passwordDB}@cluster0.c9zjxa0.mongodb.net/${nameDB}?retryWrites=true&w=majority`;
 
 function conectar(){
-    mongoose.connect(URL,  { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(mes=>{console.log("Se ha conectado con el server")}).catch(err=>console.log("error al conectar"));
+    mongoose.connect(URL,  { useNewUrlParser: true, useUnifiedTopology: true });
+    // mongoose.connect(URL,  { useNewUrlParser: true, useUnifiedTopology: true })
+    // .then(mes=>{console.log("Se ha conectado con el server")}).catch(err=>console.log("error al conectar"));
 } 
 
 module.exports={conectar}
